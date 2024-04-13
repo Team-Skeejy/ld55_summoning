4ba37694d643b519ff298eaca6b545c83f761052
mhear22 <mhear22@gmail.com>
2024-04-14 00:24:21 +1000

    Added dudes and Dude AI to the game

--- scenes/roots/game/game.gd
+++ scenes/roots/game/game.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var dude = load("res://scenes/characters/Dude.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,6 +8,23 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+var spawn_interval = 5
+var time_passed = 0
 
+func _process(delta):
+	time_passed += delta
+	if(time_passed > spawn_interval):
+		time_passed = 0
+		var new_dude = dude.instantiate()
+		
+		new_dude.parentContainer = $DudeContainer
+		var team = randi_range(1,2)
+		new_dude.team = team
+		if(team == 1):
+			new_dude.homeNode = $Home1
+			new_dude.global_position = $Home1.global_position
+		else:
+			new_dude.homeNode = $Home2
+			new_dude.global_position = $Home2.global_position
+		$DudeContainer.add_child(new_dude)
+	pass
