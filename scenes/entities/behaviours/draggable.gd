7aeb164c8e701ad9941e1ebe14e73e5957cb2727
Hedd Schotte <subzerofusion@gmail.com>
2024-04-13 19:47:25 +1000

    gravity now works with the thingo

--- scenes/entities/behaviours/draggable.gd
+++ scenes/entities/behaviours/draggable.gd
@@ -4,8 +4,9 @@ extends Node
 
 var dragging: bool = false
 var force_multiplier: float = 1000
-var deadzone: float = 1
-var deadzone_falloff: float = 50
+var torque_multiplier: float = 1000
+
+var _drag_pos: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	parent.input_event.connect(on_input_event)
@@ -15,6 +16,7 @@ func on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void
 		var ev: InputEventMouseButton = event as InputEventMouseButton
 		if ev.pressed && ev.button_index == 1:
 			dragging = true
+			_drag_pos = parent.get_local_mouse_position()
 	# elif event is InputEventScreenTouch:
 	# 	var ev: InputEventScreenTouch = event as InputEventScreenTouch
 	# 	if ev.pressed && ev.index == 1:
@@ -34,15 +36,14 @@ func _input(event: InputEvent) -> void:
 	# 		dragging = false
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if !dragging: return
-	var mouse_delta: Vector2 = parent.get_global_mouse_position() - parent.global_position
+	var drag_pos_relative_to_global: Vector2 = _drag_pos.rotated(parent.get_transform().get_rotation())
+	var mouse_delta: Vector2 = parent.get_global_mouse_position() - parent.global_position - drag_pos_relative_to_global
+	var angle: float = Vector2.UP.angle_to(drag_pos_relative_to_global)
 
+	var force: Vector2 = mouse_delta * force_multiplier
+	var torque: float = -angle * torque_multiplier
 
-	var force: Vector2 = mouse_delta * force_multiplier * delta
-	var til_deadzone: float = mouse_delta.length() - deadzone
-	var normalised_force_curve: float = clampf(til_deadzone, 0, deadzone_falloff) / deadzone_falloff
-	var falloff_applied: Vector2 = force.lerp(Vector2.ZERO, 1 - normalised_force_curve)
-
-	parent.apply_impulse(falloff_applied)
-	# parent.apply_impulse(Vector2.ONE)
+	parent.apply_force(force)
+	parent.apply_torque(torque)
